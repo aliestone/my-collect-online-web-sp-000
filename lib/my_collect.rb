5f65249c8dbd1a0178f76740a126eaa556ca3789
Alie Stone <helloaliestone@gmail.com>
2020-09-30 15:00:50 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,10 +1,12 @@
 
 
-def my_collect(collection)
+def my_collect(lang)
   collection = []
 
   while i < collection.length
-    yield collection[i]
-    i = i.capitalize
+    yield collection[lang]
+    lang = lang.capitalize
+    collection << lang
   end
+  collection
 end
